0f0f07df1e7eb9c50a1667cc0091c797d898235c
adhishrayas <96647579+adhishrayas@users.noreply.github.com>
2023-02-04 00:08:50 +0530

    more settings

--- drfecom/settings/base.py
+++ drfecom/settings/base.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
 
     #3rd party
     'rest_framework',
+
+    #internal apps
+
 ]
 
 MIDDLEWARE = [
@@ -112,3 +115,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    
+}
